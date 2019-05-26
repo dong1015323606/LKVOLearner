57fdbd7153381b45c2cd42ee3fbb04b0d9e39ebb
asheknight <1015323606@qq.com>
2019-05-26 18:21:53 +0800

    modify the test script

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,8 @@
-DATAROOT=../kitti/
+DATAROOT=/gdata/KITTI/raw_data/
 PWD=$(pwd)
-CKPT=$PWD/checkpoints/finetune/5_model.pth
+CKPT=$PWD/pre_checkpoint/kcs_posenet+ddvo.pth
 # CKPT=$PWD/posenet+ddvo.pth 
 OUTPUT=$PWD/posenet+ddvo
 CUDA_VISIBLE_DEVICES=0 nice -10 python src/testKITTI.py --dataset_root $DATAROOT --ckpt_file $CKPT --output_path $OUTPUT --test_file_list test_files_eigen.txt
 
-python2 ~/SfMLearner/kitti_eval/eval_depth.py --kitti_dir=$DATAROOT --pred_file=$OUTPUT.npy --test_file_list test_files_eigen.txt
+python2 ../SfM_Depth_Estimation/kitti_eval/eval_depth.py --kitti_dir=$DATAROOT --pred_file=$OUTPUT.npy --test_file_list test_files_eigen.txt
